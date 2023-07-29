8e369a00d41b7e148e054ffc01384bc742114661
MadhugayathriB <madhu05.marolix@2023>
2023-07-29 22:02:11 +0530

    my first commit

--- Assignmentfile.py
+++ Assignmentfile.py
@@ -59,3 +59,105 @@ if a<b:
     print(str(a) +" is smaller than b")
 else:
     print(str(b) +" is smaller than a")
+
+
+a=67
+b=67
+if a>b:
+    print(str(a)+" is greater than "+str(b))
+elif a==b:
+    print("both"+ str(a)+ "and"+ str(b) +" are equal")
+else:
+    print(str(b)+" is greater than "+str(a))
+
+
+#3 third problem: string repetition
+m="moon "
+print(m*3)
+
+s="stars "
+print(s*4)
+
+print("* "*3+s+"* "*3)
+
+r="rocket"
+print(r*5)
+
+#4 fourth prblm : print half of string 
+st="python"
+l=len(st)
+le=int(l/2)
+first_half=st[:le]
+sec_half=st[le:]
+
+
+st="marolix technology"
+le=len(st)
+for i in range(le) :
+    if st[i]==" ":
+        num=i
+first_half=st[:num]
+sec_half=st[num:]
+print(first_half)
+print(sec_half)
+
+st="training assignment"
+first=""
+sec=""
+for i in st:
+    first+=i
+    if i==" ":
+        break
+print(first)
+
+st="software coaching"
+first=""
+sec=""
+for i in st:
+    first+=i
+    if i==" ":
+        break
+
+le=len(first)
+second=st[le:]
+print(first)
+print(second)
+
+
+st="dotnet"
+l=len(st)
+le=int(l/2)
+print(st[:le])
+print(st[le:])
+
+
+#5 fifth prblm: pass or fail
+score =int(input("enter marks : "))
+if score<50:
+    print("Sorry u have been failed in the examination")
+elif score>=50 and score<80:
+    print("U have been passed in the examination ")
+
+else:
+    print("Congrats u have scored distinction")
+
+
+    #6 sixth problerm: verify if eligible to vote
+age=int(input('enter age: '))
+if age>=18:
+    print("You are eligible to vote")
+else:
+    print("Sorry , u are not eligible")
+
+
+#7 seventh prblm: square or not
+a=int(input("enter firstone : "))
+b=int(input("enter sec num : "))
+if a==b:
+    print("Hey its a square")
+else:
+    print("Its not a square")
+
+
+#8 eigth prblm: even and odd seperations
+
